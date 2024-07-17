617cb36f177d1a1d74e50e6647fdd9588a4c2d52
Nivethan Eswaran <140292846+Nivethan01@users.noreply.github.com>
2024-07-17 23:51:39 +0530

    Initial commit

--- Client/your-cart/src/Components/Product.jsx
+++ Client/your-cart/src/Components/Product.jsx
@@ -25,27 +25,31 @@ const Product = () => {
     <div id="products" className="products-list">
       <h2>Collections</h2>
       <div className="product-cards">
-        {products.map((product) => (
-          <div key={product._id} className="product-card">
-            <div className="product-image">
-              <img
-                src={`http://localhost:3001/image/${product.image.data}`}
-                alt={product.name}
-              />
-            </div>
-            <div className="product-details">
-              <div className="product-header">
-                <div className="product-brand">{product.brand}</div>
-                <div className="product-price">&#8377;{product.price}</div>
+        {products.length > 0 ? (
+          products.map((product) => (
+            <div key={product._id} className="product-card">
+              <div className="product-image">
+                <img
+                  src={`http://localhost:3001/image/${product.image.data}`}
+                  alt={product.name}
+                />
               </div>
-              <div className="product-name">{product.name}</div>
-              <div className="product-icons">
-                <img src={heartIcon} alt="Favorite" className="favorite-icon" />
-                <img src={cartIcon} alt="Add to Cart" className="cart-icon" />
+              <div className="product-details">
+                <div className="product-header">
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">&#8377;{product.price}</div>
+                </div>
+                <div className="product-name">{product.name}</div>
+                <div className="product-icons">
+                  <img src={heartIcon} alt="Favorite" className="favorite-icon" />
+                  <img src={cartIcon} alt="Add to Cart" className="cart-icon" />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>No products available</p>
+        )}
       </div>
     </div>
   );
